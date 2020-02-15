a8dc444da3e1d0b0e9adc4e8e004a9083d855208
Teng K J <16057447+TengGit@users.noreply.github.com>
2020-02-15 21:56:31 +0800

    Stylize

--- js/Customization.js
+++ js/Customization.js
@@ -1,37 +1,38 @@
 // This document is about customization
 $(document).ready(function() {
-    var switchbg=1;
-    $(':radio[name="if_bg"]').change(function(){
-        if ($(':radio[name="if_bg"]:checked').val() === "color"){
-            switchbg=0;
+	var defaultBackgroundImage = "./img/customization/default.jpg";
 
-        }else {
-            switchbg=1;
-        }
-    });
-    $("#bg_set").click(function(){
-        if(switchbg===1){
-            $("#body_bg").css({
-                "background": "url(\"" + $("#bg_preview").attr("src") + "\")  no-repeat center center",
-                "background-size": "100%"
-            });
-        }else{
-            var bgcolor = $("#bg_color").val();
-            $("#body_bg").css("background",bgcolor);
-        }
+	function useImageAsBackground() {
+		$("#bg-color-select").css("display", "none");
+		$("#bg-img-select").css("display", "block");
+		$("#body_bg").css("background", "url(\"" + $("#bg_preview").attr("src") + "\") center / cover no-repeat");
+	}
 
-    });
+	function useColorAsBackground() {
+		$("#bg-color-select").css("display", "block");
+		$("#bg-img-select").css("display", "none");
+		$("#body_bg").css("background", $("#bg_color").val());
+	}
+
+	$("#color-bg").on("change", useColorAsBackground);
+
+	$("#img-bg").on("change", useImageAsBackground);
+
     $("#select_bg_img").click(function(){
         $("#bg_img").click();
     });
-    $("#bg_preview").attr("src", "./img/customization/default.jpg");
+
+    $("#bg_preview").attr("src", defaultBackgroundImage);
     $("#bg_img").change(function () {
         var files = this.files;
         var preview = $("#bg_preview").get(0);
         if (files.length) {
             preview.src = window.URL.createObjectURL(files.item(0));
         } else {
-            preview.src ="./img/customization/default.jpg";
+            preview.src = defaultBackgroundImage;
         }
+		useImageAsBackground();
     })
+
+	$("#bg_color").change(useColorAsBackground);
 });
